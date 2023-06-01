7e1a43aded34aae63173bb657295e317db6e1128
Zuzena <98824626+Zuzena@users.noreply.github.com>
2023-06-01 12:00:25 +0300

    Checkpoint class
    Created checkpoint class

---
+++ Assets/Scripts/Environment/Checkpoint.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // check if player has triggered checkpoint
+        if (collision.transform.tag == "Player")
+        {
+            // set players last checkpoint position in player script
+            Player.checkpointPosition = transform.position;
+        }
+    }
+}
