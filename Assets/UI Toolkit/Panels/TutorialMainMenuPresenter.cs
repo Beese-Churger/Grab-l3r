266283a63ceaabe57e2eb289d8df82707e88eabe
kamkmaea <marjatuhkanen@kamk.fi>
2023-06-28 15:22:15 +0300

    First video done, tutorial buttons show debuglog press
    Simplest version on main menu iu taking shape, debug.log scriop added and working

--- Assets/UI Toolkit/Panels/TutorialMainMenuPresenter.cs
+++ Assets/UI Toolkit/Panels/TutorialMainMenuPresenter.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TutorialMainMenuPresenter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        root.Q<Button>("NewGameButton").clicked += () => Debug.Log("New Game Button clicked");
+        root.Q<Button>("LoadGameButton").clicked += () => Debug.Log("Load Game Button clicked");
+        root.Q<Button>("HighscoreButton").clicked += () => Debug.Log("Highscore Button clicked");
+        root.Q<Button>("CutscenesButton").clicked += () => Debug.Log("Cutscenes Button clicked");
+        root.Q<Button>("OptionsButton").clicked += () => Debug.Log("Options Button clicked");
+        root.Q<Button>("CreditsButton").clicked += () => Debug.Log("Credits Button clicked");
+        root.Q<Button>("QuitButton").clicked += () => Debug.Log("Quit Button clicked");
     }
 }
