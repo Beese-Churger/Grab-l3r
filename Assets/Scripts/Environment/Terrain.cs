ca1feee6f805794f74749597292840a64cb86aad
Zuzena <98824626+Zuzena@users.noreply.github.com>
2023-07-06 11:41:18 -0700

    moving platform fix

--- Assets/Scripts/Environment/Terrain.cs
+++ Assets/Scripts/Environment/Terrain.cs
@@ -18,10 +18,17 @@ public class Terrain : MonoBehaviour
     public bool triggerPressurePlate = false;
     private Vector2 startPos;
     private bool isRight;
+    private float timer;
+    private float delay = 2f;
 
-    void Start()
+    private void Start()
     {
         startPos = transform.position;
+    }
+
+    private void Awake()
+    {
+        isRight = false;
     }
 
     void Update()
@@ -29,26 +36,51 @@ public class Terrain : MonoBehaviour
         switch (terrainType)
         {
             case TerrainType.moving:
-                if (triggerPressurePlate)
+                if(triggerPressurePlate)
                 {
-                    //transform.position = Vector2.Lerp(startPos, endPos, Mathf.PingPong(Time.time * speed, 1f))
-                    transform.position=Vector2.MoveTowards(transform.position, endPos, Time.deltaTime*speed);
+                    Move();
                 }
                 break;
         }
     }
 
-    //private void MoveLeft()
-    //{
-    //    var step = speed * Time.deltaTime;
-    //    transform.position=Vector2.MoveTowards(transform.position, startPos, step);
-    //}
-
-    //private void MoveRight()
-    //{
-    //    var step=speed* Time.deltaTime;
-    //    transform.position=Vector2.MoveTowards(transform.position, endPos, step);
-    //}
+    private void Move()
+    {
+        if (!isRight)
+        {
+            if (Vector2.Distance(transform.position, endPos) < 0.001f)
+            {
+                transform.position = endPos;
+                timer += Time.deltaTime;
+                if(timer > delay)
+                {
+                    isRight = true;
+                    timer = 0;
+                }
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, endPos, Time.deltaTime * speed);
+            }
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, startPos) < 0.001f)
+            {
+                transform.position = startPos;
+                timer += Time.deltaTime;
+                if (timer > delay)
+                {
+                    isRight = false;
+                    timer = 0;
+                }
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, startPos, Time.deltaTime * speed);
+            }
+        }
+    }
 
     public void ActivateMovingPlatform()
     {
