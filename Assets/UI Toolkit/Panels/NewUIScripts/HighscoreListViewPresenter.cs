a65dc856aa916ce9d7f1e15b04e3c0a6424512a4
Zuzena <98824626+Zuzena@users.noreply.github.com>
2023-07-28 13:41:11 +0300

    Update HighscoreListViewPresenter.cs

--- Assets/UI Toolkit/Panels/NewUIScripts/HighscoreListViewPresenter.cs
+++ Assets/UI Toolkit/Panels/NewUIScripts/HighscoreListViewPresenter.cs
@@ -1,9 +1,18 @@
 using UnityEngine.UIElements;
 using System;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class HighscoreListViewPresenter
 {
     private Button _backButton;
+    private int highscore;
+
+    private void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", highscore);
+    }
+
     public Action BackAction { set => _backButton.clicked += value; }
 
     public HighscoreListViewPresenter(VisualElement root)
